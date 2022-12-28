c1a4ced2d95be66af3301462e9725f14934050cf
goldenway <goldenway777@gmail.com>
2022-12-28 19:44:40 +0200

    display mocked list of technologies

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Technology } from '../shared/technology.model';
 
 @Component({
   selector: 'pl-technologies',
   templateUrl: './technologies.component.html'
 })
-export class TechnologiesComponent {}
+export class TechnologiesComponent {
+  technologies: Technology[] = [
+    new Technology('JavaScript', 5),
+    new Technology('HTML5', 5),
+    new Technology('Angular', 4)
+  ];
+}
