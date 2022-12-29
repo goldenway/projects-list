215f6d9fdae63bcad86e327d2423d7bdf2e48981
goldenway <goldenway777@gmail.com>
2022-12-29 20:56:58 +0200

    move project's template code to separate component

--- src/app/projects/projects-list/project-item/project-item.component.ts
+++ src/app/projects/projects-list/project-item/project-item.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../project.model';
 
 @Component({
   selector: 'pl-project-item',
   templateUrl: './project-item.component.html'
 })
-export class ProjectItemComponent {}
+export class ProjectItemComponent {
+  @Input() project: Project;
+}
