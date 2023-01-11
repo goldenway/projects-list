d8cf23f3b6f509a3243d1a98db43e8ab49f39770
goldenway <goldenway777@gmail.com>
2023-01-11 17:46:15 +0200

    display a list of technologies in project details

--- src/app/projects/project.model.ts
+++ src/app/projects/project.model.ts
@@ -1,11 +1,15 @@
+import { Technology } from "../shared/technology.model";
+
 export class Project {
     public name: string;
     public description: string;
     public imagePath: string;
+    public technologies: Technology[];
 
-    constructor(name: string, description: string, imagePath: string) {
+    constructor(name: string, description: string, imagePath: string, technologies: Technology[]) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.technologies = technologies;
     }
 }
