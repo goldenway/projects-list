2afd70d9d18a4545a810e09f3a5cfb4aa0ddc917
goldenway <goldenway777@gmail.com>
2023-01-26 18:58:03 +0200

    add child routes for projects section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { ProjectsComponent } from "./projects/projects.component";
 import { TechnologiesComponent } from "./technologies/technologies.component";
+import { ProjectStartComponent } from "./projects/project-start/project-start.component";
+import { ProjectDetailComponent } from "./projects/project-detail/project-detail.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/projects', pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent },
+  { path: 'projects', component: ProjectsComponent, children: [
+    { path: '', component: ProjectStartComponent },
+    { path: ':id', component: ProjectDetailComponent }
+  ] },
   { path: 'technologies', component: TechnologiesComponent }
 ];
 
--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { TechnologiesService } from 'src/app/technologies/technologies.service';
 import { Project } from '../project.model';
+import { ProjectsService } from '../projects.service';
 
 @Component({
   selector: 'pl-project-detail',
   templateUrl: './project-detail.component.html'
 })
-export class ProjectDetailComponent {
-  @Input() project: Project;
+export class ProjectDetailComponent implements OnInit {
+  project: Project;
+  projectIndex: number;
 
-  constructor(private technologisService: TechnologiesService) {}
+  constructor(private technologisService: TechnologiesService,
+              private projectService: ProjectsService,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.projectIndex = +params['id'];
+        this.project = this.projectService.getProjectWithIndex(this.projectIndex);
+      }
+    );
+  }
 
   onRecalculateTechnologies() {
     this.technologisService.recalculateTechnologies(this.project.technologies);
