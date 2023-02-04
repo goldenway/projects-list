876bb7804fd1dd7b30244fdb12cb3765a382f968
goldenway <goldenway777@gmail.com>
2023-02-04 18:32:08 +0200

    load technologies dynamically

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Technology } from '../shared/technology.model';
 import { TechnologiesService } from './technologies.service';
 
@@ -6,12 +8,22 @@ import { TechnologiesService } from './technologies.service';
   selector: 'pl-technologies',
   templateUrl: './technologies.component.html'
 })
-export class TechnologiesComponent implements OnInit {
+export class TechnologiesComponent implements OnInit, OnDestroy {
   technologies: Technology[];
+  subscription: Subscription;
 
   constructor(private technologiesService: TechnologiesService) {}
 
   ngOnInit(): void {
     this.technologies = this.technologiesService.getTechnologies();
+    this.subscription = this.technologiesService.technologiesChanged.subscribe(
+      (technologies: Technology[]) => {
+        this.technologies = technologies;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/technologies/technologies.service.ts
+++ src/app/technologies/technologies.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Technology } from "../shared/technology.model";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class TechnologiesService {
+	technologiesChanged = new Subject<Technology[]>();
 	private technologies: Technology[] = [
 		new Technology('JavaScript', 5),
 		new Technology('HTML5', 5),
@@ -17,9 +20,11 @@ export class TechnologiesService {
 
 	addTechnology(technology: Technology) {
 		this.technologies.push(technology);
+		this.technologiesChanged.next(this.technologies.slice());
 	}
 
 	recalculateTechnologies(technologies: Technology[]) {
 		this.technologies.push(...technologies);
+		this.technologiesChanged.next(this.technologies.slice());
 	}
 }
