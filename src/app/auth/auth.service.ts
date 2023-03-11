3f2362d12595dd1317964df9a18fc2982cf5b18f
goldenway <goldenway777@gmail.com>
2023-03-11 16:03:15 +0200

    implement the auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,8 +18,10 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient,
-                private router: Router) {}
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) {}
     
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -62,6 +64,31 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+
+        // store the auth state to localStorage
+        localStorage.setItem('userData', JSON.stringify(user));
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
     }
 
     private handleError(errorResp: HttpErrorResponse) {
